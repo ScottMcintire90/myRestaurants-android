e4fc37430acf97e97b341a3379c1d61d77ea53f1
sm <scottmcintire90@gmail.com>
2016-07-06 09:26:59 -0700

    add picasso width and height restrictions

--- app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantListAdapter.java
@@ -23,8 +23,12 @@ import butterknife.ButterKnife;
 
 
 public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder> {
+    private static final int MAX_WIDTH = 200;
+    private static final int MAX_HEIGHT = 200;
+
     private ArrayList<Restaurant> mRestaurants = new ArrayList<>();
     private Context mContext;
+
 
     public RestaurantListAdapter(Context context, ArrayList<Restaurant> restaurants) {
         mContext = context;
@@ -74,7 +78,11 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
         }
 
         public void bindRestaurant(Restaurant restaurant) {
-            Picasso.with(mContext).load(restaurant.getImageUrl()).into(mRestaurantImageView);
+            Picasso.with(mContext)
+                    .load(restaurant.getImageUrl())
+                    .resize(MAX_WIDTH, MAX_HEIGHT)
+                    .centerCrop()
+                    .into(mRestaurantImageView);
             mNameTextView.setText(restaurant.getName());
             mCategoryTextView.setText(restaurant.getCategories().get(0));
             mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
